bb7d842c7a03cd2ac3c49cdf6768dc0f38677ce7
Thales <90907393+thalesamaral@users.noreply.github.com>
2025-01-26 20:07:48 -0300

    feat: init app.js | test: create items to the list

---
+++ 02-Nivel/02-Desafio-Quicklist/app.js
@@ -0,0 +1,53 @@
+const listForm = document.querySelector("form")
+const listFormInput = document.getElementById("list-input")
+const listUL = document.querySelector("ul")
+
+let allListItems = []
+
+listForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    addItem()
+})
+
+function addItem() {
+    const inputValue = listFormInput.value.trim()
+    
+    if(inputValue.length > 0) {
+        allListItems.push(inputValue)
+        // createListItem(inputValue)
+        updateList()
+        listFormInput.value = ""
+    }
+    
+    console.log(allListItems)
+}
+
+function updateList() {
+    listUL.innerText = ""
+
+    allListItems.forEach((item, itemIndex) => {
+        newItem = createListItem(item, itemIndex)
+        listUL.append(newItem)
+    })
+}
+
+function createListItem(item, itemIndex) {
+    const chkID = "chk-" + itemIndex
+    const itemLi = document.createElement("li")
+    // itemLi.innerText = item
+    
+    itemLi.innerHTML = `
+        <div class="check-box">
+            <input type="checkbox" id="${chkID}">
+        </div>
+        <label for="${chkID}">
+            ${item}
+        </label>
+        <a href="#">
+            <img src="assets/icons/garbage.svg" alt="garbage">
+        </a>
+    `
+    
+    // listUL.append(itemLi)
+    return itemLi
+}
