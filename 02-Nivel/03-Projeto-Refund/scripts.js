08c1c1ea29c224c8e9eab55f9dc34378a7c60471
Thales <90907393+thalesamaral@users.noreply.github.com>
2025-02-24 18:25:19 +0000

    feat: obtain form values and create list item, add expense icon

--- 02-Nivel/03-Projeto-Refund/scripts.js
+++ 02-Nivel/03-Projeto-Refund/scripts.js
@@ -1,12 +1,19 @@
+// Seleciona os elementos do formulário
+const form = document.querySelector("form");
 const amount = document.getElementById("amount");
+const expense = document.getElementById("expense");
+const category = document.getElementById("category");
+
+// Seleciona os elementos da lista
+const expenseList = document.querySelector("ul");
 
 // Capturando mudanças no input
 amount.oninput = () => {
   // Aceitando somente números no valor do input
   let value = amount.value.replace(/\D/g, "");
-  
+
   // Transformar o valor em centatos (exemplo: 150/100 = 1.5 é equivalente a R$ 1,50)
-  value = Number(value) / 100
+  value = Number(value) / 100;
   amount.value = formatCurrencyBRL(value);
 };
 
@@ -18,4 +25,47 @@ function formatCurrencyBRL(value) {
   });
 
   return value;
+}
+
+// Captura o evento de submit do formulário para obter os valores
+form.onsubmit = (event) => {
+  // Previne o comportamento padrão de recarregar a página
+  event.preventDefault();
+
+  // Cria um objeto com os detalhes da nova despesa
+  const newExpense = {
+    id: new Date().getTime(),
+    expense: expense.value,
+    category_id: category.value,
+    category_name: category.options[category.selectedIndex].text,
+    amount: amount.value,
+    created_at: new Date(),
+  };
+
+  console.log(newExpense);
+
+  // Chama a função que irá adicionar o item na lista
+  expenseAdd(newExpense);
+};
+
+function expenseAdd(newExpense) {
+  try {
+    // Cria o elemento para a adicionar o item (li) na lista (ul)
+    const expenseItem = document.createElement("li");
+    expenseItem.classList.add("expense");
+
+    // Cria o ícone da categoria
+    const expenseIcon = document.createElement("img");
+    expenseIcon.setAttribute("src", `img/${newExpense.category_id}.svg`);
+    expenseIcon.setAttribute("alt", newExpense.category_name);
+
+    // Adiciona as informações no item
+    expenseItem.append(expenseIcon);
+
+    // Adiciona o item na lista (li -> ul)
+    expenseList.append(expenseItem);
+  } catch (error) {
+    alert("Não foi possível atualizar a lista de despesas.");
+    console.log(error);
+  }
 }
